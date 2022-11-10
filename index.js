311210cd1c0b70dfbe186783b76943818cc0c436
sa23sa <74133458+sa23sa@users.noreply.github.com>
2022-11-10 14:15:02 +0300

    bug dicoverd

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ function mooncakeSays(message) {
   let mooncake = "(o.o)";
 
   if (!message) {
-    return `${mooncake} chookity?`;
+    return `${mooncake} ?`;
   }
 
   return `${mooncake} ${message}`;
 }
-const currentYear = new Date().getFullYear();
-const randomNumber = Math.floor(Math.random() * 5) + 1;
-const whiteColor = "#fff";
+
 module.exports.mooncakeSays = mooncakeSays;
