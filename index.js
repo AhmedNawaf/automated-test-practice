eb833879af1a569345d7e9e95c9003f92d4053eb
sa23sa <74133458+sa23sa@users.noreply.github.com>
2022-11-10 13:57:04 +0300

    2 branches merged

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function mooncakeSays(message) {
 
   return `${mooncake} ${message}`;
 }
-
+const currentYear = new Date().getFullYear();
 const randomNumber = Math.floor(Math.random() * 5) + 1;
 module.exports.mooncakeSays = mooncakeSays;
