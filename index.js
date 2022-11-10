a729984442bbe45bf18c28a013a5ce8a304017f3
sa23sa <74133458+sa23sa@users.noreply.github.com>
2022-11-10 14:16:31 +0300

    bug fixed with new feature

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function mooncakeSays(message) {
   let mooncake = "(o.o)";
 
   if (!message) {
-    return `${mooncake} ?`;
+    return `${mooncake} chookity?`;
   }
 
   return `${mooncake} ${message}`;
 }
-
+const text = "Hello";
 module.exports.mooncakeSays = mooncakeSays;
