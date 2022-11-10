1cb58064805e68425fab5136263bcf0916cf8c12
sa23sa <74133458+sa23sa@users.noreply.github.com>
2022-11-10 13:58:31 +0300

    bug discoverd

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ function mooncakeSays(message) {
   let mooncake = "(o.o)";
 
   if (!message) {
-    return `${mooncake} chookity?`;
+    return `${mooncake} ?`;
   }
 
   return `${mooncake} ${message}`;
 }
-const currentYear = new Date().getFullYear();
-const randomNumber = Math.floor(Math.random() * 5) + 1;
+
 module.exports.mooncakeSays = mooncakeSays;
