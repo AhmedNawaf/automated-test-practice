9905230b3c0df2b31f5f0405770ef5f439f04f59
sa23sa <74133458+sa23sa@users.noreply.github.com>
2022-11-10 13:59:41 +0300

    bug fixed with new feature

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ function mooncakeSays(message) {
   let mooncake = "(o.o)";
 
   if (!message) {
-    return `${mooncake} ?`;
+    return `${mooncake} chookity?`;
   }
 
   return `${mooncake} ${message}`;
 }
 
+const whiteColor = "#fff";
 module.exports.mooncakeSays = mooncakeSays;
